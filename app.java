3628ab8afcf567fc563d417d934a6867f10db67d
Pat Amoah <benjaminasiamah210@yahoo.com>
2022-02-16 14:13:11 +0000

    my first commit

---
+++ app.java
@@ -0,0 +1,8 @@
+echo "I was born great"
+echo "Enter your state"
+read state
+echo "Enter your username"
+read username
+echo "Enter your password"
+read -s password"
+echo "Welcome to Trinitech the land of opporunities".
